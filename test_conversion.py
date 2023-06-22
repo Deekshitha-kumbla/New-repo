4395e9a6c15fd6b8a33d2c940a9f9cf52fa39b79
Deekshitha-kumbla <deekshithakumbla7@gmail.com>
2023-06-22 11:08:56 +0200

    add first test_1

--- test_conversion.py
+++ test_conversion.py
@@ -1,5 +1,13 @@
+import pytest
 from conversion import celsius_to_fahrenheit
 
 def test_celsius_to_fahrenheit():
     #fahr= celsius_to_fahrenheit(20)
     assert celsius_to_fahrenheit(10)==54.5
+
+def test_celsius_to_farenheit_invalid():
+    with pytest.raises(TypeError):
+        celsius_to_fahrenheit("Invalid")
+
+def test_celsius_to_farenheit_none():
+    assert celsius_to_fahrenheit(None) is None
