affdea3d67b75a625250ae9b4629303d155542f3
Deekshitha-kumbla <deekshithakumbla7@gmail.com>
2023-06-22 10:24:55 +0200

    add first test

--- test_conversion.py
+++ test_conversion.py
@@ -2,4 +2,4 @@ from conversion import celsius_to_fahrenheit
 
 def test_celsius_to_fahrenheit():
     #fahr= celsius_to_fahrenheit(20)
-    assert celsius_to_fahrenheit(20)==68
+    assert celsius_to_fahrenheit(10)==54.5
